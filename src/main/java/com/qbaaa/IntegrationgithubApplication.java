55a86c340921c9cede3ff8425c4d468eec61d3ba
Qbaaa <jakubkowalczyk95@gmail.com>
2024-05-15 21:36:21 +0200

    project init

---
+++ src/main/java/com/qbaaa/IntegrationgithubApplication.java
@@ -0,0 +1,13 @@
+package com.qbaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationgithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qbaaa/IntegrationgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qbaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationgithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
